f87a27329b2331d993f52a003a52c0724d592728
Jack Homan <homan.jack@gmail.com>
2022-04-30 02:56:59 -0400

    adding stack to aliases

--- alias.go
+++ alias.go
@@ -1,13 +1,16 @@
 package pkg
 
 import (
+	"github.com/johnhoman/go-pkg/stack"
 	"github.com/johnhoman/go-pkg/tree/bst"
 )
 
 var (
 	NewBST = bst.New
+	Stack  = stack.New
 )
 
 var (
 	_ = NewBST
+	_ = Stack
 )
