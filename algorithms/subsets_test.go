784721b12ef5defe8a4e0965817e07397e0553d0
Jack Homan <homan.jack@gmail.com>
2022-05-01 23:37:37 -0400

    refactor subset tests

--- algorithms/subsets_test.go
+++ algorithms/subsets_test.go
@@ -17,21 +17,34 @@ func power(base, exp int) int {
 }
 
 func TestSubsets(t *testing.T) {
-    items := make([][]interface{}, 0)
 
-    set := []interface{}{1, 2}
-    Subsets(set, func(subset []interface{}) {
-        sub := make([]interface{}, len(subset))
-        reflect.Copy(reflect.ValueOf(sub), reflect.ValueOf(subset))
-        items = append(items, sub)
-        fmt.Printf("%#v\n", items)
-    })
-    expected := [][]interface{}{
-        {},
-        {1},
-        {1, 2},
-        {2},
+    tests := []struct{
+        values []interface{}
+        expected [][]interface{}
+    } {
+        {
+            []interface{}{1, 2},
+            [][]interface{}{
+                {},
+                {1},
+                {1, 2},
+                {2},
+            },
+        },
     }
-    require.Len(t, items, power(2, len(set)))
-    require.Equal(t, expected, items)
+
+    for k, subtest := range tests {
+        t.Run(fmt.Sprintf("%d", k), func(t *testing.T) {
+            items := make([][]interface{}, 0)
+
+            Subsets(subtest.values, func(subset []interface{}) {
+                sub := make([]interface{}, len(subset))
+                reflect.Copy(reflect.ValueOf(sub), reflect.ValueOf(subset))
+                items = append(items, sub)
+                fmt.Printf("%#v\n", items)
+            })
+            require.Len(t, items, power(2, len(subtest.values)))
+            require.Equal(t, subtest.expected, items)
+        })
+    }
 }
